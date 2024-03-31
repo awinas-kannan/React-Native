6395df6588063b31bb7d8ba8798ca437b48a45e1
Awinas Kannan M R <Awinas.Kannan.Manicka.Rajan@walmart.com>
2024-03-31 17:06:12 +0530

    Stack Navigation form the Doc
    Stack Navigation form the Doc

--- ReactNavigation/App.js
+++ ReactNavigation/App.js
@@ -2,15 +2,18 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import AboutScreen from './screens/About';
+import { MyStack } from './screens/StackNavigationDoc';
 
 export default function App() {
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName= "Home">
+      {/* <Stack.Navigator initialRouteName= "Home">
         <Stack.Screen name='Home' component={HomeScreen}/> 
         <Stack.Screen name='About' component={AboutScreen}/> 
-      </Stack.Navigator>
+      </Stack.Navigator> */}
+
+      <MyStack></MyStack>
     </NavigationContainer>
   );
 }
