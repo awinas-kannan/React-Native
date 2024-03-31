244ffee004bf3b4d2c374eba67e1d9f7a96ad924
Awinas Kannan M R <Awinas.Kannan.Manicka.Rajan@walmart.com>
2024-03-31 18:52:17 +0530

    Dynamic Stack Navigation Options
     Dynamic Stack Navigation Options

--- ReactNavigation/App.js
+++ ReactNavigation/App.js
@@ -2,14 +2,14 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import AboutScreen from './screens/About';
-import { MyStack, MyStackFromDoc } from './screens/StackNavigationDoc';
+import { MyStackFromDoc } from './screens/StackNavigationDoc';
 import { Alert, Pressable, Text } from 'react-native';
 
 export default function App() {
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
-      
+
       <Stack.Navigator initialRouteName="Home" screenOptions={{
         headerStyle: {
           backgroundColor: 'green'
@@ -49,7 +49,10 @@ export default function App() {
             //   backgroundColor: "lightgreen"
             // }
           }} />
-        <Stack.Screen name='About' component={AboutScreen} />
+        <Stack.Screen name='About' component={AboutScreen}
+          options={({ route }) => ({
+            title: route.params.name
+          })} />
       </Stack.Navigator>
 
       {/* <MyStackFromDoc></MyStackFromDoc> */}
