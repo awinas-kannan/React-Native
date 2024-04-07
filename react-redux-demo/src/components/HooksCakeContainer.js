7aaf7da9bed1680de32ce8de786b02828b2906b2
Awinas Kannan M R <Awinas.Kannan.Manicka.Rajan@walmart.com>
2024-04-07 17:56:35 +0530

    Update Hooks Cake container with number
    Update Hooks Cake container with number

--- react-redux-demo/src/components/HooksCakeContainer.js
+++ react-redux-demo/src/components/HooksCakeContainer.js
@@ -1,17 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { buyCake } from '../redux/cake/cakeActions'
 
 
 function HooksCakeContainer(props) {
+    const [number, setNumber] = useState(1)
     const numOfCakes = useSelector(state => state.cake.numOfCakes)
     const dispatch = useDispatch()
     return (
         <div>
             <h2>Number of cakes - {numOfCakes} </h2>
-            <button onClick={() => dispatch(buyCake())}>Buy Cake</button>
+            <input type='number' value={number} onChange={e => setNumber(e.target.value)} />
+            <button onClick={() => dispatch(buyCake(number))}>Buy Cake</button>
         </div>
     )
-}
+} 
 
 export default HooksCakeContainer
